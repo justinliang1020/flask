c2dd05b765e0c61debc830aae9834bad91821663
Justin Liang <justinliang1020@gmail.com>
2023-01-18 15:33:07 -0600

    cors

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 import os
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
